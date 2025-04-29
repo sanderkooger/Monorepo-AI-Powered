15c7124a2c469a1b9fe2a8ff5093b73b81d7cdc6
sanderkooger <sander@thisisfashion.tv>
2025-04-29 14:20:45 +0200

    added admin user for myself

--- infrastructure/opentofu/providers/vault.tf
+++ infrastructure/opentofu/providers/vault.tf
@@ -2,3 +2,39 @@ provider "vault" {
   address = var.vault_addr
   token   = var.vault_token
 }
+
+resource "vault_auth_backend" "userpass" {
+  type = "userpass"
+}
+
+resource "vault_policy" "admin" {
+  name = "admin-policy"
+  policy = <<EOT
+path "sys/*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+path "kv/Monorepo-AI-Powered/*" {
+  capabilities = ["create", "read", "update", "delete", "list"]
+}
+
+path "auth/userpass/*" {
+  capabilities = ["create", "read", "update", "delete", "list"]
+}
+EOT
+}
+
+data "vault_generic_secret" "sander_password" {
+  path = "kv/Monorepo-AI-Powered/env/sander-password"
+}
+
+resource "vault_generic_endpoint" "sander_user" {
+  depends_on           = [vault_auth_backend.userpass]
+  path                 = "auth/userpass/users/sander"
+  ignore_absent_fields = true
+
+  data_json = jsonencode({
+    policies = ["admin-policy"]
+    password = data.vault_generic_secret.sander_password.data["value"]
+  })
+}
