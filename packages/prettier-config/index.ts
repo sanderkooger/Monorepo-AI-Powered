585f2103089d99603604cf2018a5a1a455bf62c2
sanderkooger <sander@thisisfashion.tv>
2025-05-31 10:26:53 +0200

    feat: added shared prettier config

---
+++ packages/prettier-config/index.ts
@@ -0,0 +1,12 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 100,
+};
+
+export default config;
