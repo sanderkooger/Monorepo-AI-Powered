2938bd309d8aebe129ef710d86ae697da803794a
sanderkooger <sander@thisisfashion.tv>
2025-05-31 10:26:10 +0200

    feat: added nodejs config and updated packages

---
+++ packages/eslint-config/node-js.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+import { config as baseConfig } from "./base.js";
+
+/**
+ * A custom ESLint configuration for Node.js packages.
+ *
+ * @type {import("eslint").Linter.Config} */
+export const nodeJsConfig = [
+  ...baseConfig,
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+];
