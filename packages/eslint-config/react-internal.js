ffd96a2c64cc31722b320556532610a18c34499c
sanderkooger <sander@thisisfashion.tv>
2025-06-01 15:30:09 +0200

    chore: updated eslint configs

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -10,7 +10,7 @@ import { config as baseConfig } from "./base.js";
  * A custom ESLint configuration for libraries that use React.
  *
  * @type {import("eslint").Linter.Config} */
-export const config = [
+export const reactPackageConfig = [
   ...baseConfig,
   js.configs.recommended,
   eslintConfigPrettier,
